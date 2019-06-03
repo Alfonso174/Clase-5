a865c7318ab9a460a2530d2fb38fe45a6b930d52
Alfonso <alfonsocor@gmail.com>
2019-06-03 18:05:59 -0500

    actividad5

---
+++ 4promedio.js
@@ -0,0 +1,12 @@
+const Examen1= 8
+const Examen2= 6
+const Examen3= 6
+const Sumaexamen= Examen1 + Examen2 + Examen3 
+const promediodiv= Sumaexamen / 3
+    if ( promediodiv>= 8.5) {
+        console.log( "Premio")
+    } else if (promediodiv <= 5.9) {
+        console.log("Castigo")
+    } else { 
+        console.log(" no pasa nada")
+    }
